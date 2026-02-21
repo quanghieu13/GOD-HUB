82b8abf612bd59be2be86a266e940c57a15f57e5
QuangHieu? :!!! <158441412+quanghieu13@users.noreply.github.com>
2026-02-21 18:31:16 +0700

    Create GodHub.lua

---
+++ Hub2/GodHub.lua
@@ -0,0 +1,6 @@
+getgenv().ScriptTitle = "GOD HUB"
+getgenv().ScriptSubTitle = "V11.11.11"
+getgenv().ScriptImage = "https://i.ibb.co/4n5WtJCV/avater.jpg"
+getgenv().ScriptAuthorName = "GOD QUANGHIEU"
+getgenv().ScriptAuthorSubTitle = "Author Info"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/quanghieu13/GOD-HUB/refs/heads/main/Hub.lua"))()
